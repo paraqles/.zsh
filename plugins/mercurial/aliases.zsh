37ac03b5c6a1dbaebc0b3a1055ef4a965a5b6882
paraqles <paraqles@dthal.net>
2012-08-13 09:30:16 +0200

    Added aliases for mercurial branches to mecurial plugin

--- plugins/mercurial/aliases.zsh
+++ plugins/mercurial/aliases.zsh
@@ -1,6 +1,8 @@
 # Mecurial init, clone and user set aliases
 alias hgin='hg init'
 alias hgcon='hg clone'
+alias hgb='hg branch'
+alias hgbs='hg branches'
 
 # Mecurial commit aliases
 alias hgc='hg commit'
