37ed95af731da4a87faa1c946c740fc6c413f7a5
paraqles <paraqles@dthal.net>
2012-09-12 18:39:50 +0200

    Extracted aliases for rvm plugin to their own file.

---
+++ plugins/rvm/aliases.zsh
@@ -0,0 +1,17 @@
+alias rvmgl='rvm gemset list'
+alias rvmgla='rvm gemset list_all'
+alias rvmgc='rvm gemset create'
+alias rvmgu='rvm gemset use'
+alias rvmgd='rvm gemset delete'
+
+alias rvml='rvm list'
+alias rvmlk='rvm list known'
+alias rvmla='rvm list known'
+
+alias rvmu='rvm use'
+alias rvmud='rvm use --default'
+
+alias rvmi='rvm install'
+alias rvmd='rvm remove'
+alias rvmup='rvm get'
+alias rvmug='rvm upgrade'
--- plugins/rvm/rvm.zsh
+++ plugins/rvm/rvm.zsh
@@ -1,23 +1,9 @@
+ZSH_PLUGIN_RVM=$ZSH_PLUGINS/rvm
+
 PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
 
 if [[ -x $HOME/.rvm/scripts/rvm ]]; then
   . $HOME/.rvm/scripts/rvm
 fi
 
-alias rvmgl='rvm gemset list'
-alias rvmgla='rvm gemset list_all'
-alias rvmgc='rvm gemset create'
-alias rvmgu='rvm gemset use'
-alias rvmgd='rvm gemset delete'
-
-alias rvml='rvm list'
-alias rvmlk='rvm list known'
-alias rvmla='rvm list known'
-
-alias rvmu='rvm use'
-alias rvmud='rvm use --default'
-
-alias rvmi='rvm install'
-alias rvmd='rvm remove'
-alias rvmup='rvm get'
-alias rvmug='rvm upgrade'
+. $ZSH_PLUGIN_RVM/aliases.zsh
