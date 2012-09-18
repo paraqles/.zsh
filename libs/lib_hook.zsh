d3ff00a5e558ad84a3d9cb1546aa9421c07145ac
paraqles <paraqles@dthal.net>
2012-09-18 21:09:04 +0200

    Next attempt to implement a hook system.

--- libs/lib_hook.zsh
+++ libs/lib_hook.zsh
@@ -1,44 +1,58 @@
-hook_opt_set=()
-hook_opt_unset=()
-hook_prompt=()
+require lib_hash.zsh
 
-function hook_register() {
-	hook=$1
-	callback=$2
-	if [[ $hook == 'opt_set' ]]; then
-		hook_opt_set[${#hook_opt_set}+1]=$callback
-	elif [[ $hook == 'opt_unset' ]]; then
-		hook_opt_unset[${#hook_opt_unset}+1]=$callback
-	elif [[ $hook == 'prompt' ]]; then
-		hook_prompt[${#prompt}+1]=$callback
-	fi
+typeset -A hooks
+
+hook_add() {
+  if [ $# -le 2 ]; then
+    return -1
+  fi
+
+  if [ -z $hooks[$1] ]; then
+    typeset -A $hooks[$1]
+  fi
+  return 1
+}
+
+hook_register() {
+  if [[ $# -lt 2 ]]; then
+    return -1
+  fi
+
+  hook=$1
+  callback=$2
+
+  if [[ ! -z hooks[$hook] ]]; then
+    typeset -A hooks[$hook]
+  else
+    hooks[$hook][${#hooks[$hook]}+1] = callback
+  fi
 }
 
 function hook_unregister() {
-	hook=$1
-	callback=$2
-	if [[ $hook == 'opt_set' ]]; then
-		hook_opt_set[${#hook_opt_set}+1]=$callback
-	elif [[ $hook == 'opt_unset' ]]; then
-		hook_opt_unset[${#hook_opt_unset}+1]=$callback
-	elif [[ $hook == 'prompt' ]]; then
-		hook_prompt[${#prompt}+1]=$callback
-	fi
+  hook=$1
+  callback=$2
+  if [[ -z hook[$hook] ]]; then
+    hook_opt_set[${#hook_opt_set}+1]=$callback
+  elif [[ $hook == 'opt_unset' ]]; then
+    hook_opt_unset[${#hook_opt_unset}+1]=$callback
+  elif [[ $hook == 'prompt' ]]; then
+    hook_prompt[${#prompt}+1]=$callback
+  fi
 }
 
 function hook_run() {
-	hook=$1
-	argv=$2
-	callbacks=()
-	if [[ hook == 'opt_set' ]]; then
-		callbacks=$hook_opt_set
-	elif [[ hook == 'opt_unset' ]]; then
-		callbacks=$hook_opt_unset
-	elif [[ hook == 'opt_set' ]]; then
-		callbacks=$hook_prompt
-	fi
-	for callback in $callbacks; do
-		$callback $argv
-	done
+  hook=$1
+  argv=$2
+  callbacks=()
+  if [[ hook == 'opt_set' ]]; then
+    callbacks=$hook_opt_set
+  elif [[ hook == 'opt_unset' ]]; then
+    callbacks=$hook_opt_unset
+  elif [[ hook == 'opt_set' ]]; then
+    callbacks=$hook_prompt
+  fi
+  for callback in $callbacks; do
+    $callback $argv
+  done
 }
 
