d28baae474109cfdb6a74de0c933e6e7dfa6abb9
paraqles <paraqles@dthal.net>
2012-09-18 21:10:21 +0200

    Moved key_bindings to plugin linux.

--- libs/lib_keybinding.zsh
+++ libs/lib_keybinding.zsh
@@ -1,9 +1,10 @@
 bindkey -v
-bindkey '[1~'     beginning-of-line
-bindkey '[4~'     end-of-line
-bindkey '[3~'     delete-char
-bindkey '[5~'     delete-char
-bindkey '[1;5C'   forward-word
-bindkey '[1;5D'   backward-word
-bindkey '<C-BS>'  backward-kill-word
-bindkey '<C-Del>' delete-word
+# Set the keybindings specific to the OS in the plugins for them.
+#bindkey '[1~'     beginning-of-line
+#bindkey '[4~'     end-of-line
+#bindkey '[3~'     delete-char
+#bindkey '[5~'     delete-char
+#bindkey '[1;5C'   forward-word
+#bindkey '[1;5D'   backward-word
+#bindkey '<C-BS>'  backward-kill-word
+#bindkey '<C-Del>' delete-word
