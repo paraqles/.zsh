99d7be74e50a0014b79a5b1329eabb75e28ee8cd
paraqles <paraqles@dthal.net>
2018-04-27 20:19:19 +0200

    FIX: better use ps instead of pgrep -> more robust FIX: ssh-agent handling in ssh plugin

--- functions/func.find_pid.zsh
+++ functions/func.find_pid.zsh
@@ -1,10 +1,6 @@
 function find_pid {
   pattern=$1
 
-  if [[ -x $(which pgrep) ]]; then
-    echo $(pgrep -u `whoami` $pattern)
-  else
-    echo $(ps -u `whoami` -o pid,command | sed -ne "s/\([0-9]\+\) \+$pattern.*/\1/p")
-  fi
+  echo $(ps -u `whoami` -o pid,command | sed -ne "s/\([0-9]\+\) \+$pattern.*/\1/p")
 }
 
